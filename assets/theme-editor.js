e7bd6ddaa430e29d439c12d07d53a4ec28395f5c
mujeeb ansari <muzeeb100@yahoo.co.in>
2023-03-15 03:02:47 +0530

    text has been changes

--- assets/theme-editor.js
+++ assets/theme-editor.js
@@ -18,3 +18,22 @@ document.addEventListener('shopify:block:deselect', function(event) {
   const parentSlideshowComponent = event.target.closest('slideshow-component');
   if (parentSlideshowComponent.autoplayButtonIsSetToPlay) parentSlideshowComponent.play();
 });
+
+// mujeeb stat here 
+// Get all the product images
+var productImages = document.querySelectorAll('.product-single__media-item');
+
+// Listen for a change in the variant selector
+document.querySelector('.product-single__meta select').addEventListener('change', function(event) {
+  var selectedVariant = event.target.value.toLowerCase();
+  
+  // Loop through all the images and hide the ones that don't match the selected variant
+  productImages.forEach(function(image) {
+    if (image.dataset.variant === selectedVariant) {
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+    }
+  });
+});
+// mujeeb end here
